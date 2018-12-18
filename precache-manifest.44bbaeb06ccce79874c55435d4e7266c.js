b853a51551f9230b9c7aba495a90200f1831e432
Eugene Kalenik <6361645@gmail.com>
2018-12-18 22:38:19 +0300

    Updates

---
+++ precache-manifest.44bbaeb06ccce79874c55435d4e7266c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a649755320f2d5a3799e",
+    "url": "/i8_news_react_ts/static/js/runtime~main.a6497553.js"
+  },
+  {
+    "revision": "67600674949d5bdc25b6",
+    "url": "/i8_news_react_ts/static/js/main.67600674.chunk.js"
+  },
+  {
+    "revision": "03783d836442911ca812",
+    "url": "/i8_news_react_ts/static/js/1.03783d83.chunk.js"
+  },
+  {
+    "revision": "67600674949d5bdc25b6",
+    "url": "/i8_news_react_ts/static/css/main.3ae59317.chunk.css"
+  },
+  {
+    "revision": "5aee062ef9823171f7228bc43825246b",
+    "url": "/i8_news_react_ts/index.html"
+  }
+];
